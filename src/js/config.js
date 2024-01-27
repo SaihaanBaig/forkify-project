eb7d278eb576d9c10f6e972ce6059cc82ab05dab
SaihaanBaig <saihaanbaig@gmail.com>
2024-01-26 23:58:11 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a939ec5f-81e1-4211-88c0-3f89dde82105';
+export const MODEL_CLOSE_SEC = 2.5;
